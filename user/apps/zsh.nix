e77a08c67939b1e9e21ce7951c38f98520c70331
Julian Kirsch <contact@juliankirsch.me>
2024-01-14 09:35:33 +0100

    feat(nvim): NeoVIm plugins are now exclusively managed via home-manager.
    Signed-off-by: Julian Kirsch <contact@juliankirsch.me>

--- user/apps/zsh.nix
+++ user/apps/zsh.nix
@@ -3,7 +3,6 @@
 {
   programs.zsh = {
     enable = true;
-    dotDir = "/zsh";
 
     history = {
       size = 10000;
@@ -15,10 +14,5 @@
       plugins = [ "git" ];
       theme = "robbyrussell";
     };
-
-    # This command let's me execute arbitrary binaries downloaded through channels such as mason.
-    initExtra = ''
-      export NIX_LD=$(nix eval --impure --raw --expr 'let pkgs = import <nixpkgs> {}; NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker"; in NIX_LD')
-    '';
   };
 }
