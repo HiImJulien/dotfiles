afa2590e88743601b5edebe18705eef2a321d637
Julian Kirsch <contact@juliankirsch.me>
2024-01-15 03:35:41 +0100

    feat(nvim): Setup a small function to setup LD on nixOS.

--- user/apps/nvim/init.lua
+++ user/apps/nvim/init.lua
@@ -10,11 +10,26 @@ vim.g.mapleader     = " "
 
 vim.cmd.colorscheme = "catppuccin"
 
+
 -- Setup telescope
 do
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+
+    local telescope = require("telescope")
+    telescope.setup {
+        extensions = {
+            ["ui-select"] = {
+                require("telescope.themes").get_dropdown {}
+            }
+        }
+    }
+
+    -- Reload reevalute the plugin
+    ---@diagnostic disable-next-line: redefined-local
+    local telescope = require("telescope")
+    telescope.load_extension("ui-select")
 end
 
 -- Setup treesitter
@@ -26,8 +41,33 @@ do
     }
 end
 
+
+local function setup_nix_ld()
+    local job = require("plenary.job")
+    local cmd = "nix"
+    local args = {
+        "eval",
+        "--impure",
+        "--raw",
+        "--expr",
+        'let pkgs = import <nixpkgs> {}; NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker"; in NIX_LD'
+    }
+
+    local proc = job:new({
+        command = cmd,
+        args = args,
+        enable_recording = true,
+    })
+
+    proc:sync()
+    vim.env.NIX_LD = vim.trim(proc:result()[1])
+end
+
+
 -- Setup mason
 do
+    setup_nix_ld()
+
     local mason = require("mason")
     mason.setup()
 end
@@ -51,4 +91,9 @@ end
 do
     local cfg = require("lspconfig")
     cfg.lua_ls.setup({})
+
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 end
+
