6acf702a2c91118d7db7426ade0840c750720941
Julian Kirsch <contact@juliankirsch.me>
2024-01-19 16:45:30 +0100

    feat(git): Setup signing commits using SSH.
    Signed-off-by: Julian Kirsch <contact@juliankirsch.me>

--- user/apps/git.nix
+++ user/apps/git.nix
@@ -5,8 +5,13 @@
     enable = true;
     userName = "Julian Kirsch";
     userEmail = "contact@juliankirsch.me";
+    lfs.enable = true;
     extraConfig = {
       init.defaultBranch = "main";
+
+      commit.gpgsign = true;
+      gpg.format = "ssh";
+      user.signingkey = "~/.ssh/id_ed25519.pub";
     };
   };
 }
