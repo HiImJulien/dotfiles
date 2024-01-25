9b76e57b6f424dd0865f7a66bf08d41c842805f7
Julian Kirsch <contact@juliankirsch.me>
2024-01-25 18:22:25 +0100

    feat(amaterasu): Added function to clear all docker related resources.

--- user/bin/amaterasu.nix
+++ user/bin/amaterasu.nix
@@ -8,10 +8,11 @@ let
     cat <<EOM
 Usage: $(basename $0) <command>
 Commands:
-  sync-system Synchronizes the system against the provided 'configuration.nix'.
-  sync-user   Synchronizes the user configuration against the provided 'home.nix'.
-  prune       Runs the garbage collector and deletes all old generations.
-  edit        Runs the default editor in the config directory.
+  sync-system   Synchronizes the system against the provided 'configuration.nix'.
+  sync-user     Synchronizes the user configuration against the provided 'home.nix'.
+  prune         Runs the garbage collector and deletes all old generations.
+  clean-docker  Deletes ALL docker resources (Containers, Images, Volumes, Networks).
+  edit          Runs the default editor in the config directory.
 EOM
     exit 1
   }
@@ -26,6 +27,30 @@ EOM
 
   function prune {
     nix-collect-garbage -d
+  }
+
+  function clean_docker {
+    read -p "This action is destructive and cannot be undone. Are you sure? (yes/no) " yn
+
+    case $yn in
+      yes ) echo Proceeding;;
+      no ) echo Exiting...;
+          exit;;
+      * ) echo Invalid response;
+          exit 1;;
+    esac
+
+    echo Stopping all containers...;
+    docker stop $(docker ps -aq)
+    echo Removing all containers...;
+    docker rm $(docker ps -aq)
+    echo Removing all images...;
+    docker rmi -f $(docker images -qa)
+    echo Removing all volumes...;
+    docker volume rm $(docker volume ls -qf)
+    echo Removing all networks...;
+    docker network rm $(docker network ls -q)
+    echo Done cleaning docker. Exiting...;
   }
 
   function edit {
@@ -43,6 +68,8 @@ EOM
     sync_user;
   elif [ "$1" = "prune" ]; then
     prune;
+  elif [ "$1" = "clean-docker" ]; then
+    clean_docker;
   elif [ "$1" = "edit" ]; then
     edit;
   else
