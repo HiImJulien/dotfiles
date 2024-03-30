6ce4e4418e9531606a2808093da580f97db19ac5
Julian Kirsch <contact@juliankirsch.me>
2024-03-30 23:57:17 +0000

    feat(home): Created git configuration under new directory structure.

---
+++ home/git/default.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ... }:
+
+{
+  programs.git = {
+    enable = true;
+    userName = "Julian Kirsch";
+    userEmail = "contact@juliankirsch.me";
+    lfs.enable = true;
+
+    extraConfig = {
+      init.defaultBranch = "master";
+
+      commit.gpgsign = true;
+      gpg.format = "ssh";
+      user.signingkey = "~/.ssh/id_ed25519.pub";
+    };
+  };
+}
